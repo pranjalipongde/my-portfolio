22fd850e6d7fb2a8c68a198fa9b9beb23225ed70
pranjalipongde09 <pranjalipongde9@gmail.com>
2023-10-31 14:09:59 +0530

    about section changes

--- components/About.tsx
+++ components/About.tsx
@@ -20,30 +20,33 @@ const About = () => {
 
       <p className="mb-3 font-serif">
         After graduating with a degree in{" "}
-        <span className="font-medium">Electronics & Telecommunications</span>, I
-        decided to pursue my passion for programming. I enrolled in a coding
-        bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="font-medium ">Electronics & Telecommunications</span>,
+        I joined manufacturing company Funskool(India)pvt. ltd. as a{" "}
+        <span className="italic font-semibold">Electronics Engineer</span> but
+        after working for 1.5 year, i decided to pursue my passion for
+        programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">full-stack web development</span>. But i
+        have focused more towards{" "}
+        <span className="italic font-semibold">Frontend developement</span>{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        <span className="font-semibold">
+          ReactJS, HTML, CSS, Redux, Router, Node.js(basics)
+        </span>{" "}
+        and CSS design frameworks like{" "}
+        <span className="font-semibold">Tailwind CSS, Chakra UI, SASS</span>. I
+        am also familiar with TypeScript. I am always looking to learn new
+        technologies. I am currently looking for a full-time position as a{" "}
+        <span className="font-semibold"> Frontend developer.</span>
       </p>
 
       <p className="font-serif">
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className="italic">When I'm not coding</span>, I enjoy Cooking
+        food, watching movies, and painting. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        learning about <span className="font-medium">UI/UX Design</span>.
       </p>
     </motion.section>
   );
